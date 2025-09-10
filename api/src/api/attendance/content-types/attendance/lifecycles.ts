fbfb259d191807c54bd2afc55449c27830cdfece
TimDeepSkill <timwerner@deepskill.com>
2025-09-10 08:22:15 +0200

    task a implemented

--- api/src/api/attendance/content-types/attendance/lifecycles.ts
+++ api/src/api/attendance/content-types/attendance/lifecycles.ts
@@ -1,12 +1,3 @@
 "use strict";
 
-module.exports = {
-  async afterCreate(event) {
-    const { programId } = event.result;
-    await strapi.service("api::report.report").recompute(programId);
-  },
-  async afterUpdate(event) {
-    const { programId } = event.result;
-    await strapi.service("api::report.report").recompute(programId);
-  },
-};
+module.exports = {};
