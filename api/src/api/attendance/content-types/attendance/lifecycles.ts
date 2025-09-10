ea5337fad8a4c49212ecd29446fd3bb7c7b38418
TimDeepSkill <timwerner@deepskill.com>
2025-09-10 09:23:01 +0200

    implementatino

--- api/src/api/attendance/content-types/attendance/lifecycles.ts
+++ api/src/api/attendance/content-types/attendance/lifecycles.ts
@@ -1,3 +1,12 @@
 "use strict";
 
-module.exports = {};
+module.exports = {
+  async afterCreate(event) {
+    const { programId } = event.result;
+    await strapi.service("api::report.report").recompute(programId);
+  },
+  async afterUpdate(event) {
+    const { programId } = event.result;
+    await strapi.service("api::report.report").recompute(programId);
+  },
+};
